d25a5a77f36e330390e375087c442e23732c882a
Pranav Kshirsagar <128245885+ZackSnydersCode@users.noreply.github.com>
2024-02-18 02:27:12 -0800

    Initial commit

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('Hello world')
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+    return 'Hello'
+
+@app.route('/file')
+def returnTemp():
+    return render_template('index.html')
+
